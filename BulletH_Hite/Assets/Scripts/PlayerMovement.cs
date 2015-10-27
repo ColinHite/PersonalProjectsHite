9a5a3a49311c97d4a8aa05411401406e6723eb5e
unknown <10712935@CS514-03.ad.uvu.edu>
2015-10-26 19:10:29 -0600

    Started my game! Got to fix the Character movement though :p.

---
+++ BulletH_Hite/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		var move = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		{
+			GetComponent<Rigidbody2D> ().position = move * speed * Time.deltaTime;
+		}
+	}
+}
